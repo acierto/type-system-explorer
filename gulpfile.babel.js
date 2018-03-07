5f147defce1142d546ace4658961096c6bd72f07
Bogdan Nechyporenko <bnechyporenko@xebialabs.com>
2018-03-07 18:38:18 +0100

    updated

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,5 +3,6 @@ import './gulp/tasks/lint';
 import './gulp/tasks/watch';
 import './gulp/tasks/webpack';
 import './gulp/tasks/e2e';
+import './gulp/tasks/release';
 
 import './gulp/tasks/default';
