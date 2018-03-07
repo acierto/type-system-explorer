be2f0d7b3469a3c3170d92826b0cbc8169aacbfd
Bogdan Nechyporenko <bnechyporenko@xebialabs.com>
2018-03-07 18:40:10 +0100

    updated

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -3,4 +3,4 @@ import {release} from 'gulp-release-it';
 
 release(gulp);
 
-gulp.task('complete-release', gulp.series('lint', 'webpack-development', 'bump-release'));
+gulp.task('complete-release', gulp.series('lint', 'webpack-development', 'bump-complete-release'));
